7d99385c925e96847b8c36db6409e1ad9158797e
Daniil Z <idanix@list.ru>
2022-12-26 13:51:34 +0300

    Upgrade VirtualLocation creation methods

--- reactions/src/main/java/me/fromgate/reactions/util/location/VirtualLocation.java
+++ reactions/src/main/java/me/fromgate/reactions/util/location/VirtualLocation.java
@@ -2,6 +2,7 @@ package me.fromgate.reactions.util.location;
 
 import io.papermc.paper.math.BlockPosition;
 import me.fromgate.reactions.util.NumberUtils;
+import me.fromgate.reactions.util.Utils;
 import me.fromgate.reactions.util.parameter.Parameters;
 import org.bukkit.Axis;
 import org.bukkit.Bukkit;
@@ -12,7 +13,10 @@ import org.bukkit.configuration.ConfigurationSection;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Map;
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 // TODO: Use it in activators
 
@@ -23,60 +27,85 @@ import java.util.Objects;
 @SuppressWarnings("UnstableApiUsage")
 public class VirtualLocation implements BlockPosition {
     public static final VirtualLocation EMPTY = new VirtualLocation(null, null, null, null);
+    private static final Pattern LOCATION_PATTERN = Pattern.compile("(\\w+|\\*),(-?\\d+(?:\\.\\d+)?|\\*),(-?\\d+(?:\\.\\d+)?|\\*),(-?\\d+(?:\\.\\d+)?|\\*)(?:(?:,-?\\d+(?:\\.\\d+)?){2})?");
 
-    private final String world;
+    private final String worldName;
     private final Integer x;
     private final Integer y;
     private final Integer z;
 
-    public VirtualLocation(@NotNull String loc) {
-        String[] locSplit = loc.split(",", 5);
-        world = locSplit[0];
-        if (locSplit.length > 1 && NumberUtils.FLOAT.matcher(locSplit[1]).matches()) {
-            x = Double.valueOf(locSplit[1]).intValue();
-        } else x = null;
-        if (locSplit.length > 2 && NumberUtils.FLOAT.matcher(locSplit[2]).matches()) {
-            y = Double.valueOf(locSplit[2]).intValue();
-        } else y = null;
-        if (locSplit.length > 3 && NumberUtils.FLOAT.matcher(locSplit[3]).matches()) {
-            z = Double.valueOf(locSplit[3]).intValue();
-        } else z = null;
+    public static VirtualLocation of(@Nullable String worldName, @Nullable Integer x, @Nullable Integer y, @Nullable Integer z) {
+        return (worldName == null && x == null && y == null && z == null) ? EMPTY : new VirtualLocation(worldName, x, y, z);
     }
 
-    public VirtualLocation(@NotNull ConfigurationSection cfg) {
-        this(
-                cfg.getString("world"),
+    public static VirtualLocation of(@Nullable String loc) {
+        if (Utils.isStringEmpty(loc)) return EMPTY;
+        Matcher matcher = LOCATION_PATTERN.matcher(loc);
+        if (matcher.matches()) {
+            return new VirtualLocation(
+                    matcher.group(1).equals("*") ? null : matcher.group(1),
+                    matcher.group(2).equals("*") ? null : (int) NumberUtils.asDouble(matcher.group(2), 0),
+                    matcher.group(3).equals("*") ? null : (int) NumberUtils.asDouble(matcher.group(3), 0),
+                    matcher.group(4).equals("*") ? null : (int) NumberUtils.asDouble(matcher.group(4), 0)
+            );
+        } else {
+            return EMPTY;
+        }
+    }
+
+    public static VirtualLocation of(@Nullable Location loc) {
+        if (loc == null) return EMPTY;
+        return new VirtualLocation(
+                loc.getWorld().getName(),
+                loc.blockX(),
+                loc.blockY(),
+                loc.blockZ()
+        );
+    }
+
+    public static VirtualLocation fromConfiguration(@NotNull ConfigurationSection cfg) {
+        return new VirtualLocation(
+                !cfg.getString("world", "*").equals("*") ? cfg.getString("world") : null,
                 cfg.contains("x") ? cfg.getInt("x") : null,
                 cfg.contains("y") ? cfg.getInt("y") : null,
                 cfg.contains("z") ? cfg.getInt("z") : null
         );
     }
 
-    public VirtualLocation(@NotNull Parameters params) {
-        this(
-                params.getString("world"),
+    public static VirtualLocation fromParameters(@NotNull Parameters params) {
+        return new VirtualLocation(
+                !params.getString("world", "*").equals("*") ? params.getString("world") : null,
                 params.contains("x") ? params.getInteger("x") : null,
                 params.contains("y") ? params.getInteger("y") : null,
                 params.contains("z") ? params.getInteger("z") : null
         );
     }
 
-    public VirtualLocation(@Nullable String world, @Nullable Integer x, @Nullable Integer y, @Nullable Integer z) {
-        this.world = world;
+    public VirtualLocation(@Nullable String worldName, @Nullable Integer x, @Nullable Integer y, @Nullable Integer z) {
+        this.worldName = worldName;
         this.x = x;
         this.y = y;
         this.z = z;
     }
 
-    public VirtualLocation(@NotNull Location loc) {
-        this.world = loc.getWorld().getName();
-        this.x = loc.getBlockX();
-        this.y = loc.getBlockY();
-        this.z = loc.getBlockZ();
+    public @NotNull Parameters asParameters() {
+        return Parameters.fromMap(Map.of(
+                "world", format(worldName),
+                "x", format(x),
+                "y", format(y),
+                "z", format(z)
+        ));
+    }
+
+    public void toConfiguration(@NotNull ConfigurationSection cfg) {
+        cfg.set("world", format(worldName));
+        cfg.set("x", format(x));
+        cfg.set("y", format(y));
+        cfg.set("z", format(z));
     }
 
     public @Nullable String worldName() {
-        return this.world;
+        return this.worldName;
     }
 
     public @Nullable Integer virtualX() {
@@ -120,15 +149,20 @@ public class VirtualLocation implements BlockPosition {
 
     @Override
     public @NotNull VirtualLocation offset(int x, int y, int z) {
-        return new VirtualLocation(world, this.x + x, this.y + y, this.z + z);
+        return new VirtualLocation(
+                worldName,
+                x == 0 ? this.x : Integer.valueOf(blockX(x)),
+                y == 0 ? this.y : Integer.valueOf(blockY(y)),
+                z == 0 ? this.z : Integer.valueOf(blockZ(z))
+        );
     }
 
     @Override
     public @NotNull VirtualLocation offset(@NotNull Axis axis, int amount) {
         return amount == 0 ? this : switch (axis) {
-            case X -> new VirtualLocation(world, blockX(amount), y, z);
-            case Y -> new VirtualLocation(world, x, blockY(amount), z);
-            case Z -> new VirtualLocation(world, x, y, blockZ(amount));
+            case X -> new VirtualLocation(worldName, blockX(amount), y, z);
+            case Y -> new VirtualLocation(worldName, x, blockY(amount), z);
+            case Z -> new VirtualLocation(worldName, x, y, blockZ(amount));
         };
     }
 
@@ -138,11 +172,11 @@ public class VirtualLocation implements BlockPosition {
         Integer modX = face.getModX() == 0 ? x : Integer.valueOf(face.getModX() * amount + blockX());
         Integer modY = face.getModY() == 0 ? y : Integer.valueOf(face.getModY() * amount + blockY());
         Integer modZ = face.getModZ() == 0 ? z : Integer.valueOf(face.getModZ() * amount + blockZ());
-        return new VirtualLocation(world, modX, modY, modZ);
+        return new VirtualLocation(worldName, modX, modY, modZ);
     }
 
     public @NotNull Location toLocation() {
-        World world = Bukkit.getWorld(this.world);
+        World world = Bukkit.getWorld(worldName);
         return toCenter().toLocation(world == null ? Bukkit.getWorlds().get(0) : world);
     }
 
@@ -154,7 +188,7 @@ public class VirtualLocation implements BlockPosition {
     }
 
     public boolean isEmpty() {
-        return world == null && x == null && y == null && z == null;
+        return worldName == null && x == null && y == null && z == null;
     }
 
     public boolean isSimilar(@NotNull Location loc) {
@@ -165,23 +199,27 @@ public class VirtualLocation implements BlockPosition {
         return (x == null || x == xOther) &&
                 (y == null || y == yOther) &&
                 (z == null || z == zOther) &&
-                (world == null || worldOther.getName().equals(world));
+                (worldName == null || worldOther.getName().equals(worldName));
     }
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof VirtualLocation that)) return false;
-        return Objects.equals(world, that.world) && Objects.equals(x, that.x) && Objects.equals(y, that.y) && Objects.equals(z, that.z);
+        return Objects.equals(worldName, that.worldName) && Objects.equals(x, that.x) && Objects.equals(y, that.y) && Objects.equals(z, that.z);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(world, x, y, z);
+        return Objects.hash(worldName, x, y, z);
     }
 
     @Override
     public String toString() {
-        return this.world + "," + x + "," + y + "," + z + ",0,0";
+        return format(worldName) + "," + format(x) + "," + format(y) + "," + format(z) + ",0,0";
+    }
+
+    private static String format(Object obj) {
+        return obj == null ? "*" : obj.toString();
     }
 }
