7cb2dfc118c83fff9bbe6e2c9545c87f1963f0ec
Daniil Z <idanix@list.ru>
2023-01-27 15:20:19 +0300

    Fix BlockParameters filling

--- reactions/src/main/java/me/fromgate/reactions/util/parameter/BlockParameters.java
+++ reactions/src/main/java/me/fromgate/reactions/util/parameter/BlockParameters.java
@@ -1,12 +1,13 @@
 package me.fromgate.reactions.util.parameter;
 
+import me.fromgate.reactions.util.collections.Maps;
 import org.bukkit.block.Block;
 
 public final class BlockParameters extends Parameters {
     private final Block block;
 
     public BlockParameters(String param, Block block) {
-        super(param, Parameters.fromString(param).originMap());
+        super(param, Maps.caseInsensitive(Parameters.fromString(param).originMap()));
         this.block = block;
     }
 
