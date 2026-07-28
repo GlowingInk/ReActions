e39cca495aeb42925c07db886d7397cdd51e04ed
Daniil Z. <idanix@list.ru>
2026-07-28 23:37:42 +0400

    Cleanup variable sub and implement deletion

--- reactions/src/main/java/fun/reactions/commands/impl/sub/ReaVariableSub.java
+++ reactions/src/main/java/fun/reactions/commands/impl/sub/ReaVariableSub.java
@@ -8,6 +8,7 @@ import fun.reactions.ReActions;
 import fun.reactions.commands.RaCommandBase;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 
@@ -29,32 +30,25 @@ public final class ReaVariableSub extends RaCommandBase {
                                 .executes(ctx -> setVariable(ctx, ""))
                                 .then(argument("value", StringArgumentType.greedyString())
                                         .executes(ctx -> setVariable(ctx, StringArgumentType.getString(ctx, "value")))))
-                        .then(literal("delete") // TODO
-                                /*.executes(ctx -> { delete(ctx); return Command.SINGLE_SUCCESS; })*/))
+                        .then(literal("delete").executes(this::delete)))
                 .build();
     }
 
     private int help(@NotNull CommandContext<CommandSourceStack> ctx) {
         String name = StringArgumentType.getString(ctx, "name");
-        if (platform.getPersistentVariables().getVariable(null, name) == null) {
+        if (platform.getPersistentVariables().getVariable(playerOf(name), varNameOf(name)) == null) {
             return suggestCreate(ctx, "variable " + name, "Variable", name, "set");
         }
         return sendHelp(ctx, "variable " + esc(name),
-                "create", "&e[value]", "Create variable with optional&e value",
                 "show", "", "Show a variable",
-                "delete", "", "Delete a variable",
-                "set", "[value]", "Set variable to a &especified value"
+                "set", "[value]", "Set variable to a &especified value",
+                "delete", "", "Delete a variable"
         );
     }
 
     private int setVariable(@NotNull CommandContext<CommandSourceStack> ctx, @NotNull String value) {
         String name = StringArgumentType.getString(ctx, "name");
-        String[] varName = name.split(":");
-        platform.getPersistentVariables().setVariable(
-                varName.length > 1 ? varName[0] : null,
-                varName.length > 1 ? varName[1] : varName[0],
-                value
-        );
+        platform.getPersistentVariables().setVariable(playerOf(name), varNameOf(name), value);
         sendPrefixed(ctx, "Variable&a '&{name}'&r was created with the value:\n&{value}", Map.of(
                 "name", name,
                 "value", value
@@ -63,8 +57,36 @@ public final class ReaVariableSub extends RaCommandBase {
     }
 
     private int show(@NotNull CommandContext<CommandSourceStack> ctx) {
-        String value = platform.getPersistentVariables().getVariable(null, StringArgumentType.getString(ctx, "name"));
-        sendInky(ctx, value == null ? "" : esc(value)); // TODO Better message
+        String name = StringArgumentType.getString(ctx, "name");
+        String value = platform.getPersistentVariables().getVariable(playerOf(name), varNameOf(name));
+        if (value == null) {
+            sendInky(ctx, "Variable &c'" + esc(name) + "'&r doesn't exist!");
+            return Command.SINGLE_SUCCESS;
+        }
+        sendPrefixed(ctx, "Variable &a'&{name}'&r&7:\n&{value}", Map.of(
+                "name", name,
+                "value", value
+        ));
         return Command.SINGLE_SUCCESS;
+    }
+
+    private int delete(@NotNull CommandContext<CommandSourceStack> ctx) {
+        String name = StringArgumentType.getString(ctx, "name");
+        if (!platform.getPersistentVariables().removeVariable(playerOf(name), varNameOf(name))) {
+            sendInky(ctx, "Variable &c'" + esc(name) + "'&r doesn't exist!");
+            return Command.SINGLE_SUCCESS;
+        }
+        sendPrefixed(ctx, "Variable &a'" + esc(name) + "'&r was deleted.");
+        return Command.SINGLE_SUCCESS;
+    }
+
+    private static @Nullable String playerOf(@NotNull String name) {
+        int idx = name.indexOf(':');
+        return idx == -1 ? null : name.substring(0, idx);
+    }
+
+    private static @NotNull String varNameOf(@NotNull String name) {
+        int idx = name.indexOf(':');
+        return idx == -1 ? name : name.substring(idx + 1);
     }
 }
